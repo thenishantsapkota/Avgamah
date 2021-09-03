70990ffb8d9e6a49ae92fbd8c7cb5251b17ef8ae
Nishant Sapkota <snishant306@gmail.com>
2021-09-03 21:47:36 +0545

    feat: adds fun modules for the bot

--- itsnp/modules/fun.py
+++ itsnp/modules/fun.py
@@ -3,6 +3,7 @@ import hikari
 from hikari import Embed
 import aiohttp
 from datetime import datetime
+from hikari.messages import ButtonStyle
 import tanjun
 from hikari.interactions.base_interactions import ResponseType
 
@@ -26,8 +27,39 @@ async def cat_command(ctx: tanjun.abc.Context) -> None:
     embed.set_author(name=f"Here's a cat for you!")
     embed.set_image(cat_json["image"])
     embed.set_footer(text=f"Requested by {ctx.author}")
+    button = (
+        ctx.rest.build_action_row()
+        .add_button(ButtonStyle.LINK, "https://some-random-api.ml/")
+        .set_label("Source")
+        .add_to_container()
+    )
 
-    await ctx.respond(embed=embed)
+    await ctx.respond(embed=embed, component=button)
+
+
+@component.with_slash_command
+@tanjun.as_slash_command("dog", "Return a dog gif.", default_to_ephemeral=True)
+async def dog_command(ctx: tanjun.abc.Context) -> None:
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://some-random-api.ml/animal/dog") as resp:
+            dog_json = await resp.json()
+
+    embed = Embed(
+        color=0xF1C40F,
+        timestamp=datetime.now().astimezone(),
+        description=f"```{dog_json['fact']}```",
+    )
+    embed.set_author(name=f"Here's a dog for you!")
+    embed.set_image(dog_json["image"])
+    embed.set_footer(text=f"Requested by {ctx.author}")
+    button = (
+        ctx.rest.build_action_row()
+        .add_button(ButtonStyle.LINK, "https://some-random-api.ml/")
+        .set_label("Source")
+        .add_to_container()
+    )
+
+    await ctx.respond(embed=embed, component=button)
 
 
 @tanjun.as_loader
