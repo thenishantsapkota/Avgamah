5b65bf45171119d7539e0fd1fd71d9b76ccbfdbd
Nishant Sapkota <snishant306@gmail.com>
2021-09-15 10:59:10 +0545

    Hotfixes

--- itsnp/core/bot.py
+++ itsnp/core/bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import os
 import typing as t
@@ -6,8 +7,10 @@ from pathlib import Path
 
 import hikari
 from dotenv import load_dotenv
+from tortoise import Tortoise
 
 from itsnp import STDOUT_CHANNEL_ID, TEST_GUILD_ID, __version__
+from itsnp.core.tortoise_config import tortoise_config
 
 from .client import Client
 
@@ -15,18 +18,24 @@ load_dotenv()
 
 _ITSNP = t.TypeVar("_ITSNP", bound="Bot")
 
+logger = logging.getLogger("itsnp.main")
+
 
 class Bot(hikari.GatewayBot):
+    """Custom class that initiates Hikari's GatewayBot"""
+
     __slots__ = hikari.GatewayBot.__slots__ + ("client", "stdout_channel")
 
     def __init__(self: _ITSNP) -> None:
         super().__init__(token=os.environ.get("BOT_TOKEN"), intents=hikari.Intents.ALL)
 
     def create_client(self: _ITSNP) -> None:
+        """Function that creates the tanjun client"""
         self.client = Client.from_gateway_bot(self, set_global_commands=TEST_GUILD_ID)
         self.client.load_modules()
 
     def run(self: _ITSNP) -> None:
+        """Function that runs the Bot"""
         self.create_client()
 
         self.event_manager.subscribe(hikari.StartingEvent, self.on_starting)
@@ -37,11 +46,18 @@ class Bot(hikari.GatewayBot):
             activity=hikari.Activity(
                 name=f"Nothing Sus | Version {__version__}",
                 type=hikari.ActivityType.WATCHING,
-            )
+            ),
+            status=hikari.Status.IDLE,
         )
 
+    async def connect_db(self) -> None:
+        logger.info("Connecting to Database...")
+        await Tortoise.init(tortoise_config)
+        logger.info("Connected to Database.")
+
     async def on_starting(self: _ITSNP, event: hikari.StartingEvent) -> None:
-        logging.info("No Cache yet!")
+        logger.info("No Cache yet!")
+        asyncio.create_task(self.connect_db())
 
     async def on_started(self: _ITSNP, event: hikari.StartedEvent) -> None:
         self.client.scheduler.start()
@@ -52,7 +68,7 @@ class Bot(hikari.GatewayBot):
             timestamp=datetime.now().astimezone(),
         )
         # await self.stdout_channel.send(embed=embed)
-        logging.info("Bot is ready!")
+        logger.info("Bot is ready!")
 
     async def on_stopping(self: _ITSNP, event: hikari.StoppingEvent) -> None:
         embed = hikari.Embed(
@@ -61,4 +77,4 @@ class Bot(hikari.GatewayBot):
             timestamp=datetime.now().astimezone(),
         )
         # await self.stdout_channel.send(embed=embed)
-        logging.info("Bot has stopped.")
+        logger.info("Bot has stopped.")
--- itsnp/core/models.py
+++ itsnp/core/models.py
@@ -14,3 +14,33 @@ class GuildModel(Model):
 
         table = "guild"
         table_description = "Stores information about the guild"
+
+
+class TagModel(Model):
+    """Defining a tag model to store tags"""
+
+    name_tag = fields.TextField(null=True)
+    tag_value = fields.TextField(null=True)
+    guild_id = fields.BigIntField(null=True)
+
+    class Meta:
+        """Meta class to set table name and description"""
+
+        table = "tags"
+        table_description = "Stores all the tags"
+
+
+class ModerationRoles(Model):
+    """Defining a moderation roles model to store moderation roles for the server"""
+
+    id = fields.IntField(pk=True)
+    admin_role = fields.BigIntField(null=True)
+    mod_role = fields.BigIntField(null=True)
+    staff_role = fields.BigIntField(null=True)
+    guild_id = fields.BigIntField()
+
+    class Meta:
+        """Meta class to set table name and description"""
+
+        table = "staffroles"
+        table_description = "Stores StaffRoles of the server"
