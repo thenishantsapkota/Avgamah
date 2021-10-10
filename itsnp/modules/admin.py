0389e8e066bcc02fc7a8e1e7592b50174bbbe8b0
Nishant Sapkota <snishant306@gmail.com>
2021-10-10 21:48:26 +0545

    update: hotfixes to the existing code

--- itsnp/modules/admin.py
+++ itsnp/modules/admin.py
@@ -13,7 +13,12 @@ permissions = Permissions()
 
 role_group = tanjun.SlashCommandGroup(
     "role", "Set moderation roles for the server"
-).add_check(tanjun.AuthorPermissionCheck(hikari.Permissions.ADMINISTRATOR))
+).add_check(
+    tanjun.AuthorPermissionCheck(
+        hikari.Permissions.ADMINISTRATOR,
+        error_message="You need **Administrator** permission(s) to run this command.",
+    )
+)
 
 
 @role_group.with_command
