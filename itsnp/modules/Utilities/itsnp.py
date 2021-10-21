1a58f744a8ab8c92e6b9315fe048696b43deba99
Nishant Sapkota <snishant306@gmail.com>
2021-10-21 21:43:49 +0545

    refactor: moved itsnp, suggestions, and utils modules to Utilites directory

---
+++ itsnp/modules/Utilities/itsnp.py
@@ -0,0 +1,48 @@
+from datetime import datetime
+
+import hikari
+import tanjun
+from hikari import Embed
+from hikari.messages import ButtonStyle
+
+from itsnp.core.client import Client
+from itsnp.utils.courses import Courses
+
+component = tanjun.Component()
+course_obj = Courses()
+
+
+@component.with_slash_command
+@tanjun.with_own_permission_check(
+    hikari.Permissions.SEND_MESSAGES
+    | hikari.Permissions.VIEW_CHANNEL
+    | hikari.Permissions.EMBED_LINKS
+)
+@tanjun.with_str_slash_option("course_topic", "Topic the course is about")
+@tanjun.as_slash_command("freecourses", "Get a link of free courses.")
+async def freecourses_command(ctx: tanjun.abc.Context, *, course_topic: str) -> None:
+    course_dict = await course_obj.get_course(course_topic)
+    course_title = course_dict["title"].strip()
+    course_description = course_dict["description"].replace("\n", "").strip()
+    embed = Embed(
+        title="Free Udemy Course",
+        description=f"**Course Title - **{course_title}\n\n**Description - **{course_description})",
+        timestamp=datetime.now().astimezone(),
+        color=hikari.Color(0x00FF00),
+    )
+    button = (
+        ctx.rest.build_action_row()
+        .add_button(ButtonStyle.LINK, course_dict["direct_link"])
+        .set_label("Course Link")
+        .add_to_container()
+    )
+
+    embed.set_footer(text=f"Requested by {ctx.author}", icon=ctx.author.avatar_url)
+    embed.set_image(course_dict["img_link"].strip())
+
+    await ctx.respond(embed=embed, component=button)
+
+
+@tanjun.as_loader
+def load_components(client: Client) -> None:
+    client.add_component(component.copy())
---
+++ itsnp/modules/Utilities/utils.py
@@ -0,0 +1,90 @@
+import io
+
+import aiohttp
+import hikari
+import tanjun
+
+from itsnp.core import Client
+from itsnp.utils.permissions import Permissions
+
+component = tanjun.Component()
+permissions = Permissions()
+
+emoji_group = tanjun.slash_command_group("emoji", "Group that handles emoji operations")
+
+
+@emoji_group.with_command
+@tanjun.with_own_permission_check(
+    hikari.Permissions.SEND_MESSAGES
+    | hikari.Permissions.VIEW_CHANNEL
+    | hikari.Permissions.READ_MESSAGE_HISTORY
+    | hikari.Permissions.EMBED_LINKS
+    | hikari.Permissions.MANAGE_EMOJIS_AND_STICKERS
+)
+@tanjun.with_str_slash_option("name", "Name of the emoji")
+@tanjun.with_str_slash_option("url", "URL of the Emoji to be created.")
+@tanjun.as_slash_command("create", "Create an emoji in the guild")
+async def create_emoji(ctx: tanjun.abc.Context, url: str, name: str) -> None:
+    guild = ctx.get_guild()
+    await permissions.staff_role_check(ctx, guild)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as r:
+            try:
+                if r.status in range(200, 299):
+                    emj = io.BytesIO(await r.read())
+                    bytes = emj.getvalue()
+                    emoji = await ctx.rest.create_emoji(
+                        ctx.get_guild(), name, image=bytes
+                    )
+                    await ctx.respond(f"I created the emoji successfully | {emoji}")
+                else:
+                    await ctx.respond(f"Error making request | Response: {r.status}")
+            except hikari.BadRequestError:
+                await ctx.respond("File size may be too big.")
+
+
+@emoji_group.with_command
+@tanjun.with_own_permission_check(
+    hikari.Permissions.SEND_MESSAGES
+    | hikari.Permissions.VIEW_CHANNEL
+    | hikari.Permissions.READ_MESSAGE_HISTORY
+    | hikari.Permissions.EMBED_LINKS
+    | hikari.Permissions.MANAGE_EMOJIS_AND_STICKERS
+)
+@tanjun.with_str_slash_option("new_name", "New Name of the Emoji")
+@tanjun.with_str_slash_option("emoji_id", "ID of the Emoji to be renamed")
+@tanjun.as_slash_command("rename", "Rename an emoji of the server")
+async def rename_emoji(ctx: tanjun.abc.Context, emoji_id: str, new_name: str) -> None:
+    guild = ctx.get_guild()
+    await permissions.staff_role_check(ctx, guild)
+    emoji = await ctx.rest.fetch_emoji(guild, emoji_id)
+    await ctx.respond(
+        f"I changed the name of the emoji from `{emoji.name}`to `{new_name}`"
+    )
+    await ctx.rest.edit_emoji(guild, emoji, name=new_name)
+
+
+@emoji_group.with_command
+@tanjun.with_own_permission_check(
+    hikari.Permissions.SEND_MESSAGES
+    | hikari.Permissions.VIEW_CHANNEL
+    | hikari.Permissions.READ_MESSAGE_HISTORY
+    | hikari.Permissions.EMBED_LINKS
+    | hikari.Permissions.MANAGE_EMOJIS_AND_STICKERS
+)
+@tanjun.with_str_slash_option("emoji_id", "ID of the Emoji to be deleted")
+@tanjun.as_slash_command("delete", "Delete an emoji from the server")
+async def delete_emoji(ctx: tanjun.abc.Context, emoji_id: str) -> None:
+    guild = ctx.get_guild()
+    await permissions.staff_role_check(ctx, guild)
+    emoji = await ctx.rest.fetch_emoji(guild, emoji_id)
+    await ctx.respond(f"I deleted the emoji sucessfully | {emoji}")
+    await ctx.rest.delete_emoji(guild, emoji)
+
+
+component.add_slash_command(emoji_group)
+
+
+@tanjun.as_loader
+def load_components(client: Client) -> None:
+    client.add_component(component.copy())
