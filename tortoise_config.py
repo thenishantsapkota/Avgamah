2a5d01c09ad0af20eab0c921f9d93474d86e051e
washbin <76929116+washbin@users.noreply.github.com>
2021-10-29 22:27:52 +0545

    feat: add contanerization support for better development experince

--- tortoise_config.py
+++ tortoise_config.py
@@ -5,7 +5,7 @@ tortoise_config = {
             "engine": "tortoise.backends.asyncpg",
             "credentials": {
                 "database": "bot_db",
-                "host": "localhost",
+                "host": "db",
                 "password": "test123",
                 "port": 5432,
                 "user": "nishant",
