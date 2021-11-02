56041f9a3bca5a303231e96c88f3f387dca84b5a
Nishant Sapkota <snishant306@gmail.com>
2021-11-02 14:56:59 +0545

    feat: added helper functions for adding buttons to every single message

--- itsnp/utils/buttons.py
+++ itsnp/utils/buttons.py
@@ -1,4 +1,6 @@
+import hikari
 import tanjun
+import yuyo
 from hikari.api.special_endpoints import ActionRowBuilder
 from hikari.messages import ButtonStyle
 
@@ -12,3 +14,28 @@ def create_source_button(ctx: tanjun.abc.Context, source: str) -> ActionRowBuild
     )
 
     return button
+
+
+async def delete_message_button(ctx: yuyo.ComponentContext) -> None:
+    if (
+        ctx.interaction.message
+        and ctx.interaction.message.interaction.user.id == ctx.interaction.user.id
+    ):
+        await ctx.defer(hikari.ResponseType.DEFERRED_MESSAGE_UPDATE)
+        await ctx.delete_initial_response()
+
+    else:
+        await ctx.create_initial_response(
+            hikari.ResponseType.MESSAGE_CREATE,
+            f"This button belongs to someone else.",
+            flags=hikari.MessageFlag.EPHEMERAL,
+        )
+
+
+DELETE_CUSTOM_ID = "AUTHOR_DELETE_BUTTON"
+DELETE_ROW = (
+    hikari.impl.ActionRowBuilder()
+    .add_button(hikari.ButtonStyle.DANGER, DELETE_CUSTOM_ID)
+    .set_emoji("\N{HEAVY MULTIPLICATION X}\N{VARIATION SELECTOR-16}")
+    .add_to_container()
+)
