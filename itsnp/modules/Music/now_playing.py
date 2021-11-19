a8159452a469910a385e766eddbf696a72af65a2
Nishant Sapkota <snishant306@gmail.com>
2021-11-19 18:58:04 +0545

    refactor: changes in `nowplaying` command(minor)

--- itsnp/modules/Music/now_playing.py
+++ itsnp/modules/Music/now_playing.py
@@ -1,8 +1,12 @@
+from datetime import timedelta
+
 import hikari
+import lavasnek_rs
 import tanjun
 
 from itsnp.core.client import Client
 from itsnp.utils.buttons import DELETE_ROW
+from itsnp.utils.time import pretty_timedelta_shortened
 
 now_playing_component = tanjun.Component()
 
@@ -17,12 +21,18 @@ now_playing_component = tanjun.Component()
 )
 @tanjun.as_slash_command("nowplaying", "See Currently Playing Song")
 async def now_playing(ctx: tanjun.abc.Context) -> None:
-    node = await ctx.shards.data.lavalink.get_guild_node(ctx.guild_id)
+    node: lavasnek_rs.Node = await ctx.shards.data.lavalink.get_guild_node(ctx.guild_id)
 
     if not node or not node.now_playing:
         return await ctx.respond(
             "There's nothing playing at the moment!", component=DELETE_ROW
         )
+    total = pretty_timedelta_shortened(
+        timedelta(seconds=node.now_playing.track.info.length / 1000)
+    )
+    now = pretty_timedelta_shortened(
+        timedelta(seconds=node.now_playing.track.info.position / 1000)
+    )
 
     embed = hikari.Embed(
         title="Now Playing",
@@ -32,6 +42,7 @@ async def now_playing(ctx: tanjun.abc.Context) -> None:
     fields = [
         ("Requested by", f"<@{node.now_playing.requester}>", True),
         ("Author", node.now_playing.track.info.author, True),
+        ("Duration", f"{now}/{total}", True),
     ]
     for name, value, inline in fields:
         embed.add_field(name=name, value=value, inline=inline)
