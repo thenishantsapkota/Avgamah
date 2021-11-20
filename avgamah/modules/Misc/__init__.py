7f7d7709408292cea148f4ad6186326598ab59a1
Nishant Sapkota <snishant306@gmail.com>
2021-11-20 21:10:25 +0545

    fix: hotfix

--- avgamah/modules/Misc/__init__.py
+++ avgamah/modules/Misc/__init__.py
@@ -1,8 +1,8 @@
 application_ids = {
     "PokerNight": 755827207812677713,
-    "Betrayal.io": 773336526917861400,
+    "Betrayal": 773336526917861400,
     "Youtube": 755600276941176913,
-    "Fishington.io": 814288819477020702,
+    "Fishington": 814288819477020702,
     "Doodle Crew": 878067389634314250,
     "Word Snacks": 879863976006127627,
     "Spell Cast": 852509694341283871,
