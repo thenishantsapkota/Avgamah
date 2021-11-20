91a4cfb9e946d6ace19c387a00420f7704046d36
Nishant Sapkota <snishant306@gmail.com>
2021-11-20 20:58:26 +0545

    refactor: added some more minigames

--- avgamah/modules/Misc/__init__.py
+++ avgamah/modules/Misc/__init__.py
@@ -3,6 +3,10 @@ application_ids = {
     "Betrayal.io": 773336526917861400,
     "Youtube": 755600276941176913,
     "Fishington.io": 814288819477020702,
+    "Doodle Crew": 878067389634314250,
+    "Word Snacks": 879863976006127627,
+    "Spell Cast": 852509694341283871,
+    "Letter Tile": 879863686565621790,
 }
 
 activities = list(application_ids.keys())
