13d43cf6f8836ad280c39327801b9c400fd4b3a3
Nishant Sapkota <snishant306@gmail.com>
2021-11-20 15:30:56 +0545

    refactor: made some design changes and added total playtime of the queue

--- avgamah/modules/Music/queue.py
+++ avgamah/modules/Music/queue.py
@@ -1,11 +1,11 @@
-from datetime import datetime, timedelta
+from datetime import datetime, time, timedelta
 
 import hikari
 import tanjun
 
 from avgamah.core.client import Client
 from avgamah.utils.pagination import paginate
-from avgamah.utils.time import pretty_timedelta_shortened
+from avgamah.utils.time import pretty_timedelta, pretty_timedelta_shortened
 from avgamah.utils.utilities import _chunk
 
 from . import fetch_lavalink
@@ -24,6 +24,7 @@ queue_component = tanjun.Component()
 @tanjun.as_slash_command("queue", "Shows the music queue")
 async def queue(ctx: tanjun.abc.Context) -> None:
     song_queue = []
+    length = 0
     lavalink = fetch_lavalink(ctx)
     node = await lavalink.get_guild_node(ctx.guild_id)
 
@@ -31,6 +32,7 @@ async def queue(ctx: tanjun.abc.Context) -> None:
         return await ctx.respond("There are no tracks in the queue!")
 
     for song in node.queue:
+        length += song.track.info.length
         song_queue += [
             f"`{pretty_timedelta_shortened(timedelta(seconds=song.track.info.length))}` [{song.track.info.title}]({song.track.info.uri}) [<@{song.requester}>]"
         ]
@@ -42,7 +44,7 @@ async def queue(ctx: tanjun.abc.Context) -> None:
             f"No tracks in the queue.\n**Now Playing** : [{node.now_playing.track.info.title}]({node.now_playing.track.info.uri})"
         )
     for index, track in enumerate(_chunk(song_queue[1:], 8)):
-        string = """"""
+        string = """**Next Up**\n"""
         temp = []
         for i in track:
             string += f"""**{counter})** {i}\n"""
@@ -55,6 +57,12 @@ async def queue(ctx: tanjun.abc.Context) -> None:
             description=string,
         )
         embed.set_footer(text=f"Page {index+1}")
+        embed.add_field(
+            name="Playtime",
+            value=pretty_timedelta(timedelta(seconds=length / 1000)),
+            inline=True,
+        )
+        embed.add_field(name="Entries", value=len(song_queue[1:]), inline=True)
         embed.add_field(
             name="Now Playing",
             value=f"[{node.now_playing.track.info.title}]({node.now_playing.track.info.uri}) [<@{node.now_playing.requester}>]",
