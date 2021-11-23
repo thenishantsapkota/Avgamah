c151b89b023563384fa6df2683f9bfab1247dbbf
Nishant Sapkota <snishant306@gmail.com>
2021-11-23 20:39:08 +0545

    refactor: made zodiac sign compulsory

--- avgamah/modules/Misc/rashifal.py
+++ avgamah/modules/Misc/rashifal.py
@@ -11,7 +11,7 @@ rashifal_component = tanjun.Component()
 
 @rashifal_component.with_slash_command
 @tanjun.with_str_slash_option(
-    "zodiac", "Your zodiac Sign", choices=[sign for sign in zodiac_signs], default=None
+    "zodiac", "Your zodiac Sign", choices=[sign for sign in zodiac_signs]
 )
 @tanjun.as_slash_command("rashifal", "View your today's rashifal.")
 async def rashifal(ctx: tanjun.abc.Context, zodiac: str) -> None:
